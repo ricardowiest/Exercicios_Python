755334e3dac323cfe010b4fe4237a194f7d82a05
Ricardo Wiest <ricardowiest@gmail.com>
2020-06-29 08:46:46 -0300

    Update funçõesI_somaImposto.py

--- funçõesI_somaImposto.py
+++ funçõesI_somaImposto.py
@@ -1,12 +1,12 @@
 def somaImposto(nomeImposto, taxaImposto, custo):
     saldoLiquido = (custo * taxaImposto) / 100
-    return f'O imposto {nomeImposto} com saldo bruto {custo} terá alicota de {taxaImposto}: R${saldoLiquido}'
+    return f'O imposto {nomeImposto} com saldo bruto R${custo} terá alicota de {taxaImposto}: R${saldoLiquido}'
 
 
 lista = list()
 print('\033[1:31:40mPROGRAMA PARA CALCULO DE IMPOSTOS\033[m')
 while True:
-    esc = int(input('Escola uma opção: \n[1] Cadastrar\n[2] Excluir\n[3] Ver todos\n[4] Sair')
+    esc = int(input('Escola uma opção: \n[1] Cadastrar\n[2] Excluir\n[3] Ver todos\n[4] Sair\nESCOLHA: '))
     if esc == 1:
         imposto = str(input('Digite qual imposto deseja incluir: '))
         valor = float(input('Qual o valor liquido antes do imposto: R$'))
@@ -16,8 +16,8 @@ while True:
     elif esc == 2:
         lista.pop
     elif esc == 3:
-        for c in lista:
-            print(f'{c}\n')
+        for c,k in enumerate(lista):
+            print(f'{k}')
     elif esc == 4:
         print('Finalizando...')
         break
